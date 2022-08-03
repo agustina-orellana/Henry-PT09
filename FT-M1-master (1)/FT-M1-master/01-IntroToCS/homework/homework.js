ed2b84d65204f1fe9a6ff6e136e69ef90f1ea40c
agustina <agucha.orellana1@gmail.com>
2022-08-02 22:47:44 -0300

    resolucion de ejercicios

--- FT-M1-master (1)/FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master (1)/FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,11 +2,37 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+//imput "110"
+// output 6
+// Convertir el string en array
+// [1, 1, 0]
+// Dar vuelta el array 
+// invertir el array usando esa posicion 
+// usar la posicion como factor para elevar la base a ese factor
+// multiplicar el resltado por el elemento del array us
+// sumar el resultado 
+var data = num.split("").reverse();
+var result = 0;
+for(var i = 0; i < data.length; i++){
+  result += 2** i * data[i];
 
+}
+return result; 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+// dividir num entre 2 hasta que llegue a 0
+// quedarme con el rsultado entero (sin el valor decimal) y el resto 
+// recordar los restos
+var result= "";
+
+while(num > 0){
+var result =  (num % 2) + result;
+var r = Math.floor(num/2) 
+}
+return result;
+
 
 }
 
